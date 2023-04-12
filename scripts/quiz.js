3e79dd11106a8a3bbbc54b5339f1dfdeda249e0a
hau23 <67587869+hau23@users.noreply.github.com>
2023-04-13 04:12:56 +0800

    Update error handling for quiz

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -13,16 +13,24 @@ function checkAnswer(questionType, questionNumber) {
   //question type 1 = short answer question, type 2 = mc
   if (questionType === 1) {
     userAnswer = document.querySelector(`[name=${CSS.escape(questionNumber)}`).value.toString();
+    console.log(userAnswer);
+    if (userAnswer === "") {
+      feedback.innerText = "Please input your answer!";
+      feedback.style.color = "red";
+      event.preventDefault();
+    }
 
   } else if (questionType === 2) {
     userAnswer = document.querySelector(`input[name=${CSS.escape(questionNumber)}]:checked`).value;
+  } else {
+    feedback.innerHTML = "Please select one answer!";
   }
 
-  if (userAnswer === correctAnswer[questionNumber - 1]) {
+  if (userAnswer === correctAnswer[questionNumber - 1] && userAnswer !== "") {
     feedback.innerHTML = "Correct!";
     feedback.style.color = "green";
     event.preventDefault();
-  } else {
+  } else if (userAnswer !== ""){
     attempts[questionNumber - 1]++;
     if (attempts[questionNumber - 1] < 3) {
       feedback.innerText = `Incorrect. You have ${3 - attempts[questionNumber - 1]
@@ -31,7 +39,7 @@ function checkAnswer(questionType, questionNumber) {
       event.preventDefault();
     }
     //if all attempts fail, the buttons will become unusable 
-    else {
+    else if (userAnswer !== "") {
       feedback.innerHTML = `Incorrect. The correct answer is ` + correctAnswer[questionNumber - 1];
       feedback.style.color = "red";
       document.getElementsByName(questionNumber).disabled = true;
